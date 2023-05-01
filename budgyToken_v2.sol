43611c219f5a21969eb62283eb5672f2d9dd6480
duangel <angela.du@uzh.ch>
2023-05-01 03:23:34 +0200

    v3

--- budgyToken_v2.sol
+++ budgyToken_v2.sol
@@ -8,31 +8,27 @@ pragma solidity >=0.8.10;
 import "./interfaces/IERC20.sol";
 import "./libraries/SafeMath.sol";
 
-struct User {
-    uint256 id;
+contract BudgyToken2 is IERC20 {
+
+  struct User{
+    address userData;
     // other stuff
+    bool userExists; // This boolean is used to differentiate between unset and zero struct values
+  }
 
-    bool set; // This boolean is used to differentiate between unset and zero struct values
-}
-
-contract BudgyToken is IERC20 {
   using SafeMath for uint256;
 
-  uint256 public constant _totalSupply = 10**18;
-  string public override constant name = 'Budgy Token';
+  uint256 public _totalSupply = 10**18;
+  string public override constant name = 'Budgy Token 2';
   uint8 public override constant decimals = 10;
-  string public override constant symbol = 'BDGY';
+  string public override constant symbol = 'BDGY2';
   string private constant secretMessage = 'You have unlocked the secret message. The secret to happiness are bird seeds.';
   uint256 public unitsOneEthCanBuy  = 42;
-  address[] userIDs;
 
+  mapping (address => User) public users;
   mapping (address => uint256) private _balances;
-
   mapping (address => mapping (address => uint256)) private _allowed;
-  
-  mapping(address => User) public users;
-
-  address public tokenOwner;
+  address private tokenOwner;
 
   constructor () {
     tokenOwner = msg.sender;  
@@ -40,13 +36,16 @@ contract BudgyToken is IERC20 {
     //_balances[msg.sender] = _totalSupply;
     //emit Transfer(address(0), msg.sender , _totalSupply);
     emit Transfer(address(0), tokenOwner , _totalSupply);
+    bool addNew = newUser(tokenOwner);
+    if(addNew){
+      updateUserData(tokenOwner);
+    }
   }
 
   receive() external payable {
     // code taken from https://levelup.gitconnected.com/minting-your-own-erc-20-tokens-in-ethereum-a477bd38c135    
-
     uint256 amount = msg.value * unitsOneEthCanBuy;
-
+    
     require(balanceOf(tokenOwner) >= amount, 
         "Not enough tokens");
 
@@ -55,32 +54,48 @@ contract BudgyToken is IERC20 {
     emit Transfer(tokenOwner, msg.sender, amount);
 
     payable(tokenOwner).transfer(msg.value);
-    }
-
-
-  function totalSupply() external override pure returns (uint256) {
-    return _totalSupply;
   }
 
-  
+  function isUser(address _address) public view returns(bool _isUser) {
+    return users[_address].userExists;
+  }
+
+  function newUser(address _address) public returns(bool success) {
+    // Make sure admin doesn't already exist and validate _address
+    require(!isUser(_address) && _address != address(0));
+    users[_address].userData = _address;
+    users[_address].userExists = true;
+    return true;
+  }
+
+  function updateUserData(address _address) public returns(bool success) {
+    require(isUser(_address));
+    users[_address].userData = _address;
+    return true;
+  }
+
+
+  function totalSupply() external override view returns (uint256) {
+    return _totalSupply;
+  }
 
   function balanceOf(address owner) public override view returns (uint256) {
     return _balances[owner];
   }
 
-  function createUser(address _userAddress, uint256 _userId) public returns (address[] memory){
-    User storage user = users[_userAddress];
+  //function createUser(address _userAddress, uint256 _userId) public returns (address[] memory){
+    //User storage user = users[_userAddress];
     // Check that the user did not already exist:
-    require(!user.set);
+    //require(!user.set);
     //Store the user
-    users[_userAddress] = User({
-        id: _userId,
-        set: true
-    });
-    userIDs.push(_userAddress);
+    //users[_userAddress] = User({
+      //  id: _userId,
+        //set: true
+    //});
+    //userIDs.push(_userAddress);
 
-    return userIDs;
-  }
+    //return userIDs;
+  //}
 
   /**
   * Function to check the amount of tokens that an owner allowed to a spender.
@@ -96,6 +111,10 @@ contract BudgyToken is IERC20 {
     _balances[msg.sender] = _balances[msg.sender].sub(value);
     _balances[to] = _balances[to].add(value);
     emit Transfer(msg.sender, to, value);
+    bool addNew = newUser(to);
+    if(addNew){
+      updateUserData(to);
+    }
     return true;
   }
 
@@ -107,17 +126,25 @@ contract BudgyToken is IERC20 {
   }
 
   function _transfer(address from, address to, uint value) private {
-      _balances[from] = _balances[from].sub(value);
-      _balances[to] = _balances[to].add(value);
-      emit Transfer(from, to, value);
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    emit Transfer(from, to, value);
+    bool addNew = newUser(to);
+    if(addNew){
+      updateUserData(to);
+    }  
     }
 
-  function transferFrom( address from, address to, uint256 value) public override returns (bool){
+  function transferFrom(address from, address to, uint256 value) public override returns (bool){
     require(value <= _balances[from]);
     require(value <= _allowed[from][msg.sender]);
     require(to != address(0));
     _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
     _transfer(from, to, value);
+    bool addNew = newUser(to);
+    if(addNew){
+      updateUserData(to);
+    }
     return true;
   }
 
@@ -139,27 +166,37 @@ contract BudgyToken is IERC20 {
     return true;
   }
 
-  function randomGenerator(uint256 number) public view returns (uint256){
-      return uint(keccak256(abi.encodePacked(block.timestamp,block.difficulty, msg.sender))) % number;
+  function mint(address to, uint256 amount) public {
+    require(to != address(0), "ERC20: mint to the zero address");
+    _totalSupply += amount;
+    unchecked {
+    // Overflow not possible: balance + amount is at most totalSupply + amount, which is checked above.
+    _balances[to] += amount;
+    }
+    emit Transfer(address(0), to, amount);
   }
   
-  function array_length() public view returns(uint256) {  
-        uint256 x = userIDs.length;
-        return x; 
-    } 
-  
 
-  function VIPOnly(address owner, address friend) public returns(string memory){
-      uint256 numOfOGTokens = _balances[tokenOwner];
-      uint256 numOfUsers = array_length();
-      if (numOfUsers % 5 == 0 && numOfOGTokens >= 3){
-
-          transferFrom(tokenOwner, friend, 2);
-          transferFrom(tokenOwner, owner, 1);
-
-          return secretMessage;
+  function VIPClub(address referee, address referred)public view returns(bool){
+    require(referee!=address(0));
+    require(isUser(referee));
+    if (!isUser(referred)){
+        return (true);
       }
-      return 'Sorry, better luck next time getting into our flock!';
+    return (false);
+  }
+
+  function addBirdFriends(address referee, address referred) public returns(string memory){
+    require(isUser(referee));
+    require(VIPClub(referee, referred));
+    if (!isUser(referred)){
+      mint(referred,5);
+      mint(referee,10);
+      newUser(referred);
+      updateUserData(referred);
+      return secretMessage;
     }
+    return 'Sorry, better luck next time getting into our flock!';
+  }
 
 }
